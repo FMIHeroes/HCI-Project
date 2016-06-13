a60eaa84e4e1ffb887e11923e525b9d0ab07203c
HuterInTheStorm <hunterinthestorm@gmail.com>
2016-06-13 22:05:52 +0300

    moved images to folder

--- RegUserPopUp.js
+++
@@ -1,9 +0,0 @@
-(function() {  
-    var dialog = document.getElementById('window');  
-    document.getElementById('reg').onclick = function() {  
-        dialog.show();  
-    };  
-    document.getElementById('exit').onclick = function() {  
-        dialog.close();  
-    };  
-})();
